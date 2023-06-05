ace4eec2077269d7a0f14eaadb3c5b12d84dd8dc
kapish-patel <kpatelkapish@gmail.com>
2023-06-04 19:17:19 -0700

    js files

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -89,9 +89,23 @@ class OrderModel {
             }
         });
     }
-    getAllOrderOfUser(userId) {
-        console.log(userId);
-        console.log("all orders of user");
+    getAllOrderOfUser(userdata, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const userId = userdata.userId;
+                const filter = { customerId: userId };
+                console.log(filter);
+                const userOrder = yield this.model.find(filter).exec();
+                console.log("all orders of user");
+                res.json(userOrder);
+                res.status(200);
+            }
+            catch (error) {
+                console.log("Error retrieving user orders:", error);
+                console.error(error);
+                res.sendStatus(500);
+            }
+        });
     }
 }
 exports.OrderModel = OrderModel;
